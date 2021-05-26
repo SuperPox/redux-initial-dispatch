1baa80c78c25ee10636ad5d59aebb7491bc033a0
SuperPox <calebkohl@gmail.com>
2021-05-25 20:20:52 -0600

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,19 +1,27 @@
-let state = {count: 0};
-
-function changeState(state, action){
-    switch (action.type) {
-      case 'INCREASE_COUNT':
-        return {count: state.count + 1}
-      default:
-        return state;
-    }
+let state;
+function reducer(state = {count: 0}, action){
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return {count: state.count + 1}
+    default:
+      return state;
   }
+}
 
 function dispatch(action){
-    state = changeState(state, action)
-    render()
+  state = reducer(state, action);
+  render();
 }
 
 function render(){
-    document.body.textContent = state.count
+  let container = document.getElementById('container');
+  container.textContent = state.count;
 }
+
+dispatch({type: '@@INIT'})
+
+let button = document.getElementById('button');
+
+button.addEventListener('click', () => {
+  dispatch({type: 'INCREASE_COUNT'})
+})
